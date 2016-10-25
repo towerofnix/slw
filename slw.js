3a5ad38e9499cf4c19d6e8257555b7f943aac70e
liam4 <towerofnix@gmail.com>
2016-10-25 15:18:34 -0300

    Fill in the blanks!

--- slw.js
+++ slw.js
@@ -5,10 +5,27 @@
 
 class SLW {
   constructor() {
+    this.keys = {}
+
     this.canvas = document.createElement('canvas')
+    this.canvas.width = 150
+    this.canvas.height = 150
+    this.canvas.style.border = '1px solid black'
+    this.canvas.addEventListener('keydown', evt => {
+      this.keys[evt.keyCode] = true
+    })
+    this.canvas.addEventListener('keyup', evt => {
+      this.keys[evt.keyCode] = false
+    })
+    this.canvas.setAttribute('tabindex', 1)
 
     this.playerX = 0
     this.playerY = 0
+
+    this.cameraX = 0
+    this.cameraY = 0
+
+    this.tileSize = 30
 
     this.activeLevel = {
       tiles: (
@@ -16,34 +33,56 @@ class SLW {
         '************\n'+
         '************\n'+
         '*******DDDDD\n'+
-        'DDDDD*****D*'
+        'DDDDD*****D*\n'+
+        '****D*****DD\n'+
+        '****D*******'
       )
     }
+  }
+
+  canvasClear() {
+    const ctx = this.canvas.getContext('2d')
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
+  getDrawnPosition(tileX, tileY) {
+    // Fill in the blanks!
+    return [___, ___]
   }
 
   drawLevelTiles() {
     const ctx = this.canvas.getContext('2d')
 
-    const rows = this.activeLevel.tiles.split('\n')
-    let viewStartX = this.playerX - 5
-    let viewStartY = this.playerY - 5
-    let viewEndX = viewStartX + 10
-    let viewEndY = viewStartY + 10
-    viewStartX = Math.max(viewStartX, 0)
-    viewStartY = Math.max(viewStartY, 0)
-    viewEndX = Math.max(viewEndX, 0)
-    viewEndY = Math.max(viewEndY, 0)
+    // Fill in the blanks!
+    const viewStartX = ___
+    const viewStartY = ___
+    const viewEndX = ___
+    const viewEndY = ___
 
     for (let y = viewStartY; y < viewEndY; y++) {
       for (let x = viewStartX; x < viewEndX; x++) {
         let row = rows[y] || ''
         let tile = row[x] || ''
-        console.log(x, y, tile)
 
         if (tile === 'D') {
-          ctx.fillRect(x * 30, y * 30, 30, 30)
+          ctx.fillStyle = 'red'
+        } else if (tile === '*') {
+          ctx.fillStyle = 'green'
+        } else {
+          ctx.fillStyle = 'orange'
         }
+
+        const [rendX, rendY] = this.getDrawnPosition(x, y)
+        ctx.fillRect(rendX, rendY, 30, 30)
       }
     }
+
+    ctx.fillStyle = 'blue'
+    const [pRendX, pRendY] = this.getDrawnPosition(this.playerX, this.playerY)
+    ctx.fillRect(pRendX, pRendY, 30, 30)
+
+    console.log(
+      // 'Looped tiles:', (viewEndX - viewStartX) * (viewEndY - viewStartY),
+      'Drawn tiles:', drawnTiles)
   }
 }
